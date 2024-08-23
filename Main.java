c6a9e1980744c957de0adc1ad17b570a1931acb3
Bruno De Castro <163913166+browndark@users.noreply.github.com>
2024-08-23 19:42:23 -0300

    Atividade 3

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("====Alunos: Bruno Custodio, Marlon Silva e Victor Fernandes=====");
+        for (int i = 1; i <= 100; i++){
+            if (i % 10 ==0) {
+                System.out.println(i + " é múltiplo de 10.");
+
+            }else {
+                System.out.println(i);
+            }
+        }
+    }
+}
